a9e5c814436929e5ac1375a76225d08ec699a980
Sanket <sanketparekh17@gmail.com>
2021-10-10 10:38:59 +0530

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-
-//==> tic tic toc project related data..
+//==> Calculator project related data..
 
 import React from 'react'
-import './P-2( tic tic toc )/App.css'
-import Game from './P-2( tic tic toc )/componet/Game'
+import Calculator from './P-1( Calculator )/Calculator'
 
 export default class App extends React.Component {
     render() {
         return (
-            <Game/>
+            <div>
+                <h1>Wellcome Back, my user</h1>
+                <Calculator/>
+            </div>
         )
     }
 }
+
+//==> tic tic toc project related data..
+
+// import React from 'react'
+// import './P-2( tic tic toc )/App.css'
+// import Game from './P-2( tic tic toc )/componet/Game'
+
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//             <Game/>
+//         )
+//     }
+// }
